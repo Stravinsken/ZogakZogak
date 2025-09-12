bb3142c92c58f4cbef6f681311deeeea417c210c
Stravinsken <Jsc78tt33@gmail.com>
2025-09-13 04:54:01 +0900

    fix: Correct CORS configuration

--- src/main/java/com/example/PieceOfPeace/config/WebConfig.java
+++ src/main/java/com/example/PieceOfPeace/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // 모든 경로에 대해
-                .allowedOrigins("*") // 모든 외부 출처를 허용
+                .allowedOriginPatterns("*") // allowedOrigins("*") 대신 이 방식을 사용합니다.
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS") // 모든 HTTP 메소드를 허용
                 .allowedHeaders("*") // 모든 헤더를 허용
                 .allowCredentials(true); // 쿠키/인증 정보 허용
