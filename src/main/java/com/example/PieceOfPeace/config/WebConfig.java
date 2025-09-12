7960af5ace9a1f3d1deecf60b4aeb1ae33f65d84
Stravinsken <Jsc78tt33@gmail.com>
2025-09-13 07:00:06 +0900

    fix: new domain

--- src/main/java/com/example/PieceOfPeace/config/WebConfig.java
+++ src/main/java/com/example/PieceOfPeace/config/WebConfig.java
@@ -9,11 +9,10 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**") // 모든 경로에 대해
-                // 특정 출처만 명시적으로 허용합니다.
-                .allowedOrigins("http://localhost:3000", "http://localhost:5173", "http://54.180.125.150") 
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS") // 모든 HTTP 메소드를 허용
-                .allowedHeaders("*") // 모든 헤더를 허용
-                .allowCredentials(true); // 쿠키/인증 정보 허용
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000", "http://localhost:5173", "http://54.180.125.150", "https://mingjaam.github.io")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
