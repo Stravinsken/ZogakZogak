93db550ffcd72827bfd891412c3789dd5c8ab895
Stravinsken <Jsc78tt33@gmail.com>
2025-09-13 20:35:43 +0900

    feat: senior에 ãswagger 예시 추가

--- src/main/java/com/example/PieceOfPeace/user/dto/SeniorCreateRequestDto.java
+++ src/main/java/com/example/PieceOfPeace/user/dto/SeniorCreateRequestDto.java
@@ -1,8 +1,10 @@
 package com.example.PieceOfPeace.user.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 
 @Getter
 public class SeniorCreateRequestDto {
+    @Schema(description = "등록할 어르신의 이름", example = "김철수")
     private String name;
 }
--- src/main/java/com/example/PieceOfPeace/user/dto/SeniorResponseDto.java
+++ src/main/java/com/example/PieceOfPeace/user/dto/SeniorResponseDto.java
@@ -1,12 +1,18 @@
 package com.example.PieceOfPeace.user.dto;
 
 import com.example.PieceOfPeace.user.entity.Senior;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
+@Schema(description = "어르신 정보 응답 DTO")
 public class SeniorResponseDto {
+
+    @Schema(description = "어르신 고유 식별자", example = "1")
     private final Long id;
+
+    @Schema(description = "어르신 이름", example = "이영희")
     private final String name;
 
     @Builder
